ad775cdaeca91a46f6abc35fcb4b7429b94da514
Yi Yao <yi@qtul.com>
2013-09-08 07:54:02 -0700

    Added script and documentation

--- README.md
+++ README.md
@@ -2,3 +2,73 @@ axes_swapper
 ============
 
 Tool to swap axes in G code (e.g. to wrap 2.5D program around rotary axis)
+
+
+Author
+------
+
+Yi Yao, Founder of Qtul Enterprises
+http://qtul.com/
+
+
+License
+-------
+
+    Tool to swap axes in G code (e.g. to wrap 2.5D program around rotary axis)
+    Copyright (C) 2013 Qtul Enterprises (http://qtul.com/)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+Usage
+-----
+
+axis_swapper.py Circumference InFile OutFile
+
+Where:
+* Circumference is the length in the Y axis which corresponds to a 360 degrees rotation
+* InFile is the file which will be converted
+* OutFile is the file where all Y axis references are converted to A axis and scaled
+
+
+For example:
+
+```shell
+axis_swapper.py 5 Test_input.ngc Test_output.ngc
+```
+
+
+Details
+-------
+
+This tool will swap the Y axis to A axis in a G code program. This is useful for
+wrapping 2.5D G code programs around a rotary axes.
+
+This program partially handles feedrate (F word) scaling. F words are
+interpretted as the pythagorean mean of feedrates of all axes. This is fine when
+only linear axes are concerned, but is incorrect if you have a rotary axis. For
+example, 1 inch per second usually does not equal to 1 degree per second. If F
+words are not scaled, the result is usually unbearably slow G code. This program
+handles F word scaling, but it is not numerically accurate. For most cases, the
+result is close enough and doesn't matter.
+
+This program does all its parsing conversion through a bunch of regular
+expressions.
+
+Future improvements should include:
+* Numerically correct F word scaling
+* GUI
+* Abitrary axes to axes swap
+* Handling of circular toolpaths (G2 and G3)
---
+++ axis_swapper.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+#Script to convert a 2.5D G code file to a rotary axis
+#Specifically, this converts the Y axis to A axis
+#A axis needs to be negated to maintain mirror correctness
+
+import math
+import re
+import sys
+
+
+if len(sys.argv) != 4:
+	print len(sys.argv)
+	print('Usage:')
+	print(sys.argv[0] + ' Circumference InFile OutFile')
+	print("""Where:
+\tCircumference is the length in the Y axis which corresponds to a 360 degrees rotation
+\tInFile is the file which will be converted
+\tOutFile is the file where all Y axis references are converted to A axis and scaled
+""")
+	sys.exit(1)
+
+#Parse arguments
+Circ = float(sys.argv[1])
+InFile = open(sys.argv[2], 'r')
+OutFile = open(sys.argv[3], 'w')
+
+#Verify dimensions and conversion with user
+print('Circumference = ' + str(Circ) + '\tDiameter = ' + str(Circ / math.pi))
+print('1 length unit = ' + str(360 / Circ) + ' degrees\t1 degree = ' + str(Circ / 360) + ' length units')
+
+#Search for all Y words and replace them with A words
+FeedRate = AngularFeedRate = 0
+for line in InFile:
+	#A combined linear and rotational axis move has a (dimensionally) incorrect tangential feed rate
+	#For the most part this is not a significant issue since the linear axis dominate numerically
+	#When the rotational axis move appears by itself, the feed rate causes the axis to move unbearably slow
+	#Because of this, we have to assign angular feed rates when the rotational axis appears by itself
+	#Likewise, in all other cases, we must assign tangential feed rates
+
+	#Update F word (tangential feed rate)
+	Matches = re.search(r'[fF]([-+]?[0-9]*\.?[0-9]+)\s*[a-zA-z]*', line)
+	if Matches:
+		FeedRate = float(Matches.group(1))
+		#Calculate angular feed rate
+		AngularFeedRate = FeedRate * 360 / Circ
+
+	#Swap Y and A words
+	Matches = re.search(r'[yY]([-+]?[0-9]*\.?[0-9]+)\s*[a-zA-z]*', line)
+	if Matches:
+		#Calculate A word
+		Angle = -float(Matches.group(1)) * 360 / Circ
+		#Replace Y word with A word
+		line = re.sub(r'[yY][-+]?[0-9]*\.?[0-9]+', 'A' + str(Angle), line)
+
+	#Add or replace F word
+	#If linear axes are present ...
+	if re.search(r'[xX][-+]?[0-9]*\.?[0-9]+', line) or re.search(r'[zZ][-+]?[0-9]*\.?[0-9]+', line):
+		#... and no F word is specified ...
+		if not re.search(r'[fF]', line):
+			#... then we should add our own F word (tangential feed rate)
+			line = line.strip() + ' F' + str(FeedRate) + '\n'
+	#No linear axes are present ...
+	else:
+		#... but there might be a rotational axis by itself
+		if re.search(r'[aA][-+]?[0-9]*\.?[0-9]+', line):
+			#Replace the F word if it exists
+			if re.search(r'[fF]', line):
+				line = re.sub(r'[fF][-+]?[0-9]*\.?[0-9]+', 'F' + str(AngularFeedRate), line)
+			#Add it to the end if it doesn't
+			else:
+				line = line.strip() + ' F' + str(AngularFeedRate) + '\n'
+
+	OutFile.write(line)
+
